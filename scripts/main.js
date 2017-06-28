5e8222b4e3737f21d22e8713901fff301e49bcfa
Josh Goldstein <josh.goldstein@autodesk.com>
2017-06-28 15:45:17 -0600

    Adding spacer and bonus div functionality

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,7 @@ function generateHashtags()
     if (carCheck === "Yes" || carCheck === "yes" || carCheck === "y" || carCheck === "Y" || carCheck === "yes ")
         {
         // get car data 
-        var carData = prompt("Enter the year, make, and model");
+        carData = prompt("Enter the year, make, and model");
         console.log("Variable carData = " + carData);
         carDataSplit = carData.split(" ");
         console.log("Variable carDataSplit = " + carDataSplit);
@@ -48,13 +48,81 @@ generateHashtags();
 
 function bodyHTML() 
 {
-    var div = document.createElement("div");
-    var carHashHTML = document.createTextNode(finalCarHashtags);
-    //var carHashHTML = div.innerHTML(carHash);
-    div.setAttribute("style", "background-color: pink;");
-    div.appendChild(carHashHTML);
-    document.body.appendChild(div);
+    function headerDiv()
+    {
+        var headerDiv = document.createElement("div");
+        headerDiv.className = "header";
+        var headerText = document.createTextNode("Hashtag Generator");
+        headerDiv.appendChild(headerText);
+        document.body.appendChild(headerDiv);
+    }
+    headerDiv();
+
+    function firstDiv() 
+    {
+        firstDiv = document.createElement("div");
+        firstDiv.className = "firstDiv";
+
+        if (carData)
+        {
+            var firstDivText = document.createTextNode(finalCarHashtags);
+        } 
+        else
+        {
+            var firstDivText = document.createTextNode(finalGenericHashtags);
+        }
+
+        firstDiv.appendChild(firstDivText);
+        document.body.appendChild(firstDiv);
+    }
+    firstDiv();
+
+    function spacerDiv()
+    {
+        var spacerDiv = document.createElement("div");
+        spacerDiv.className = "spacerDiv";
+
+        var spacerSymbol = ".";
+        var spacerQuantity = 3;
+
+        function spacerLines(spacerSymbol, spacerQuantity) 
+        {
+            for (i = 0; i < spacerQuantity; i++)
+            {
+                spacerDiv = document.createElement("div");
+                spacerDiv.className = "spacerDiv";
+                var spacerDivText = document.createTextNode(spacerSymbol);
+                spacerDiv.appendChild(spacerDivText);
+                document.body.appendChild(spacerDiv);
+            }
+        }
+        spacerLines(spacerSymbol, spacerQuantity);
+    }
+    spacerDiv();
+
+    function bonusDiv()
+    {
+        bonusDiv = document.createElement("div");
+        bonusDiv.className = "bonusDiv";
+
+        finalCarBonusHashtags = "#fun"
+        finalGenericBonusHashtags = ['#instagood', '#igers']
+
+        if (carData)
+        {
+            var bonusDivText = document.createTextNode(finalCarBonusHashtags);
+        } 
+        else
+        {
+            var bonusDivText = document.createTextNode(finalGenericBonusHashtags);
+        }
+
+        bonusDiv.appendChild(bonusDivText);
+        document.body.appendChild(bonusDiv);
+    }
+    bonusDiv();
+
 }
 
-// execute the body
+// execute the body HTML
 bodyHTML();
